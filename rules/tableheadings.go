0f829d113a860995b30a32be60968d95012683b4
Maldris <sdark.arrow@gmail.com>
2018-06-15 12:19:46 +1000

    extended table header rule to allow setting a span size mode and column ratios

--- rules/tableheadings.go
+++ rules/tableheadings.go
@@ -1,13 +1,29 @@
 package rules
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/golang-commonmark/markdown"
 )
 
 type TableHeader struct {
-	lvl   int
-	Lines bool
+	lvl    int
+	Lines  bool
+	Size   SizeMode
+	Cols   []float64
+	Colsum float64
 }
+
+type SizeMode int
+
+const (
+	SizeUndefined SizeMode = iota // 0
+	SizeWrap                      // 1
+	SizeHalf                      // 2
+	SizeFull                      // 3
+)
 
 func (j *TableHeader) Tag() string {
 	return "thead"
@@ -56,13 +72,52 @@ func RuleTableSettings(s *markdown.StateBlock, startLine, endLine int, silent bo
 		return true
 	}
 
-	var lines bool = false
-	if src[pos+6] == 't' || src[pos+6] == 'l' {
-		lines = true
+	tok := TableHeader{
+		Lines: true,
+		Size:  SizeWrap,
+		Cols:  []float64{},
+	}
+
+	exprs := strings.Split(src[pos+6:s.BMarks[startLine+1]], " ")
+
+	for _, exp := range exprs {
+		exp = strings.Trim(exp, " \r\n\t")
+		switch {
+		case strings.HasPrefix(exp, "l"):
+			tok.Lines = exp == "lt" || exp == "ll"
+		case strings.HasPrefix(exp, "s"):
+			switch exp {
+			case "shalf":
+				tok.Size = SizeHalf
+			case "sfull":
+				tok.Size = SizeFull
+			case "swrap":
+				fallthrough
+			default:
+				tok.Size = SizeWrap
+			}
+		case strings.HasPrefix(exp, "c"):
+			exp = exp[1:]
+			ts := strings.Split(exp, ":")
+			sum := float64(0)
+			for _, t := range ts {
+				if t == "" {
+					continue
+				}
+				f, err := strconv.ParseFloat(t, 64)
+				if err != nil {
+					fmt.Printf("[docgen] Error parsing column size: %v", err)
+					return
+				}
+				tok.Cols = append(tok.Cols, f)
+				sum += f
+			}
+			tok.Colsum = sum
+		}
 	}
 
 	s.Line = startLine + 1
-	s.PushToken(&TableHeader{Lines: lines})
+	s.PushToken(&tok)
 
 	return true
 }
